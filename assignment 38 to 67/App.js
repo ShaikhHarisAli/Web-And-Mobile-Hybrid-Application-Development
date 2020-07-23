620d822792d8bfd686517a1e598ceee74a5f73b6
ShaikhHarisAli <shaikhharisali16@gmail.com>
2020-07-23 17:11:12 -0400

    Core JavaScript Concepts

---
+++ assignment 38 to 67/App.js
@@ -0,0 +1,188 @@
+// CHAPTER 38-43
+
+// QUESTION NO 1
+// function square() {
+//     alert("Result is ; " + result)
+// }
+// n1 = parseInt(prompt("Enter Value base"))
+// n2 = parseInt(prompt("Enter Value power"))
+// var result=Math.pow(n1,n2);
+// square("Result is ; " + result)
+
+
+// QUESTION NO 2
+// var y = prompt("Enter a year")
+// function check () 
+// {
+// if( y % 4==0 )
+// {
+//     alert( y + " is a leap year")
+// }
+// else
+//  {
+//     alert( y + " is not leap year")
+// }
+// }
+// check()
+
+
+// QUESTION NO 3
+// var a = 20
+// var b = 20
+// var c = 20
+// function areaOfTriangle(){
+// console.log(d)
+// } 
+// var d = (a+b+c)/2
+// areaOfTriangle(d)
+
+
+// QUESTION NO 4
+// function getNumber(a,b,c){
+// var a =parseInt(prompt("Give mark of 1st subject")) 
+// var b =parseInt(prompt("Give mark of 2nd subject"))
+// var c =parseInt(prompt("Give mark of 3rd subject"))
+// var c1 = a+b+c
+// // console.log( " Marks get by student : " + a , b, c )
+// console.log( " Marks get by student : " + c1 )
+// function averageMarks(){
+// var d = (a+b+c)/300
+// console.log( " Average Marks : " + d)
+
+// function percentage(){
+// var totalMarks = 300
+// var percen = (d/totalMarks)*100
+// console.log(  "percentage : " + percen + "%" )
+// }
+// percentage()
+
+// } 
+// averageMarks()
+// } 
+// getNumber()
+
+
+// QUESTION NO 5
+// function findIndex(){
+// var a = prompt("Enter word")
+// var a1 = a.indexOf("a")
+// console.log(a1)
+// }
+// findIndex()
+
+// QUESTION NO 6
+// function removeVowels(a,a1){
+// var a = prompt("Enter a Sentence")
+// var a1 =a.replace(/[aeiouAEIOU]/ig , "")
+// console.log(a1)
+// }
+// console.log(removeVowels())
+
+
+// QUESTION NO 7
+// function removeVowels(a,a1){
+// var a = prompt("Enter a Sentence")
+// var a1 =a.match(/[ae]/ig)
+// console.log( a)
+// console.log("such occurrances are " + a1)
+// }
+// console.log(removeVowels())
+
+
+
+// QUESTION NO 8
+// function distanceKm(){
+// var twoCitiesdistance = 100; 
+// console.log(  " Distance in Km : " + twoCitiesdistance + " Km ")
+
+// function distanceMeters(){
+// var a = twoCitiesdistance*1000
+// console.log( "Distance in Meters : " + a + " m ")
+
+// function distanceFeet(){
+// var distanceFeet = twoCitiesdistance*3281
+// console.log( "Distance in Feet : " + distanceFeet + " ft ")
+
+// function distanceInches(){
+//     var distanceInches = twoCitiesdistance*393700
+//     console.log( "Distance in Inches : " + distanceInches + " inc ")
+
+// function distanceCenti(){
+// var distanceCenti = twoCitiesdistance*100000
+// console.log( "Distance in Centi : " + distanceCenti + " cm ")
+    
+// }    
+// distanceCenti()
+// }
+// distanceInches()
+// } 
+// distanceFeet()
+// }
+// distanceMeters()
+// }
+// distanceKm()
+
+
+// QUESTION NO 9
+// function overtimePay(){
+// var a = parseInt(prompt(" Your Extra Working Hours"))
+// var b = 12
+// var c = a*b 
+// console.log(" Extra working Hours : " + a)
+// console.log( " Your total payment is : " + c + " Rs/- ")
+// }
+// overtimePay()
+
+
+// CHAPTER 43-48
+//Question 3
+// function removerow(){
+//     var table = document.getElementById("table")
+//     table.deleteRow(1)
+// }
+
+  
+// QUESTION NO 4
+
+// function changeImage(id, src){
+// var image= document.getElementById("img");
+// image.src = src
+// }
+
+// QUESTION NO 5
+// function button1(){
+// var increment = document.getElementById("here").innerHTML
+// increment++
+// document.getElementById("here").innerHTML = increment;
+// }
+
+// function button2(){
+//     var decrement = document.getElementById("here").innerHTML
+//     decrement--
+//     document.getElementById("here").innerHTML = decrement;
+//  }
+
+
+// CHAPTER 49-52
+
+// QUESTION NO 1
+// function getInfo(){
+// var a = document.getElementById("name").value
+// var b = document.getElementById("password").value
+// console.log( "Your name is : " + a)
+// console.log( "Your password is :" + b)
+// }
+
+
+// QUESTION NO 2
+
+// function readMore(){
+// var text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus non et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus non et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus non et "
+// var paragraph = document.getElementById("para")
+// paragraph.innerHTML = text
+// }
+
+
+
+
+
